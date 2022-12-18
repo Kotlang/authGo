7befb15f598b5defd9b4835bc4775fa8bde895f7
Prince <this.is.kumar.prince@gmail.com>
2022-12-18 14:04:36 +0530

    feat(ProfileMaster): bulk getting profile master (#4)
    * feat(ProfileMaster): bulk getting profile master
    
    * feat(ProfileMaster): deleting profile master service
    
    * feat(ProfileMaster): adding new profile master service
    
    * feat(ProfileMaster): update service added
    
    * feat(ProfileMaster): only admin can access the profile master crud API
    
    * fix(auth-model): commit issue

--- service/profile_master_service.go
+++ service/profile_master_service.go
@@ -2,13 +2,16 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/Kotlang/authGo/db"
 	pb "github.com/Kotlang/authGo/generated"
+	"github.com/Kotlang/authGo/models"
 	"github.com/SaiNageswarS/go-api-boot/auth"
 	"github.com/SaiNageswarS/go-api-boot/logger"
 	"github.com/jinzhu/copier"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -45,5 +48,151 @@ func (s *ProfileMasterService) GetProfileMaster(ctx context.Context, req *pb.Get
 	case err := <-profileMasterListErrorChan:
 		logger.Error("Failed getting profile master list", zap.Error(err))
 		return nil, status.Error(codes.Internal, "Failed getting profile master list")
+	}
+}
+
+// ADMIN PORTAL API
+func (s *ProfileMasterService) BulkGetProfileMaster(ctx context.Context, req *pb.BulkGetProfileMasterRequest) (*pb.ProfileMasterResponse, error) {
+	userId, tenant := auth.GetUserIdAndTenant(ctx)
+
+	loginModelChan, errChan := s.db.Login(tenant).FindOneById(userId)
+	select {
+	case loginModel := <-loginModelChan:
+		fmt.Println(loginModel.Phone)
+		if loginModel.UserType != "admin" {
+			return nil, status.Error(codes.PermissionDenied, "User with id"+userId+" don't have permission")
+		}
+	case err := <-errChan:
+		logger.Error("Failed getting login info using id: "+userId, zap.Error(err))
+		return nil, status.Error(codes.Internal, "Failed getting login info using id: "+userId)
+	}
+
+	profileMasterListChan, profileMasterListErrorChan := s.db.ProfileMaster(tenant).Find(bson.M{}, nil, 0, 0)
+	list := make([]*pb.ProfileMasterProto, 0)
+
+	select {
+	case profileMasterList := <-profileMasterListChan:
+		copier.CopyWithOption(&list, &profileMasterList, copier.Option{DeepCopy: true})
+		return &pb.ProfileMasterResponse{
+			ProfileMasterList: list,
+		}, nil
+	case err := <-profileMasterListErrorChan:
+		logger.Error("Failed bulk getting profile master list", zap.Error(err))
+		return nil, status.Error(codes.Internal, "Failed bulk getting profile master list")
+	}
+}
+
+// ADMIN PORTAL API
+// Delete Profile Master
+func (s *ProfileMasterService) DeleteProfileMaster(ctx context.Context, req *pb.DeleteProfileMasterRequest) (*pb.DeleteProfileMasterResponse, error) {
+	userId, tenant := auth.GetUserIdAndTenant(ctx)
+
+	loginModelChan, errChan := s.db.Login(tenant).FindOneById(userId)
+	select {
+	case loginModel := <-loginModelChan:
+		if loginModel.UserType != "admin" {
+			return nil, status.Error(codes.PermissionDenied, "User with id"+userId+" don't have permission")
+		}
+	case err := <-errChan:
+		logger.Error("Failed getting login info using id: "+userId, zap.Error(err))
+		return nil, status.Error(codes.Internal, "Failed getting login info using id: "+userId)
+	}
+
+	profileMasterChan, errChan := s.db.ProfileMaster(tenant).FindOneById(req.Id)
+
+	select {
+	case profileMaster := <-profileMasterChan:
+		err := <-s.db.ProfileMaster(tenant).DeleteById(profileMaster.Id())
+
+		if err != nil {
+			logger.Error("Internal error when deleting Profile Master with id: "+req.Id, zap.Error(err))
+			return nil, status.Error(codes.Internal, err.Error())
+		} else {
+			return &pb.DeleteProfileMasterResponse{
+				Status: "success",
+			}, nil
+		}
+	case err := <-errChan:
+		logger.Error("Profile Master not found", zap.Error(err))
+		return nil, status.Error(codes.NotFound, "Profile Master not found")
+	}
+}
+
+// ADMIN PORTAL API
+// Add Profile Master
+func (s *ProfileMasterService) AddProfileMaster(ctx context.Context, req *pb.AddProfileMasterRequest) (*pb.ProfileMasterProto, error) {
+	userId, tenant := auth.GetUserIdAndTenant(ctx)
+
+	loginModelChan, errChan := s.db.Login(tenant).FindOneById(userId)
+	select {
+	case loginModel := <-loginModelChan:
+		if loginModel.UserType != "admin" {
+			return nil, status.Error(codes.PermissionDenied, "User with id"+userId+" don't have permission")
+		}
+	case err := <-errChan:
+		logger.Error("Failed getting login info using id: "+userId, zap.Error(err))
+		return nil, status.Error(codes.Internal, "Failed getting login info using id: "+userId)
+	}
+
+	if len(strings.TrimSpace(req.Language)) == 0 {
+		logger.Error("Language is not present")
+		return nil, status.Error(codes.InvalidArgument, "Language is not present")
+	}
+	profileMaster := &models.ProfileMasterModel{}
+	copier.CopyWithOption(profileMaster, req, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+
+	err := <-s.db.ProfileMaster(tenant).Save(profileMaster)
+
+	if err != nil {
+		logger.Error("Internal error when saving Profile Master with id: "+profileMaster.Id(), zap.Error(err))
+		return nil, status.Error(codes.Internal, err.Error())
+	} else {
+		profileMasterChan, errChan := s.db.ProfileMaster(tenant).FindOneById(profileMaster.Id())
+		select {
+		case profileMaster := <-profileMasterChan:
+			profileMasterProto := &pb.ProfileMasterProto{}
+			copier.Copy(profileMasterProto, profileMaster)
+			return profileMasterProto, nil
+		case err := <-errChan:
+			logger.Error("After saving, the Profile Master not found", zap.Error(err))
+			return nil, status.Error(codes.NotFound, "After saving, the Profile Master not found")
+		}
+	}
+}
+
+// ADMIN PORTAL API
+// Update Profile Master
+func (s *ProfileMasterService) UpdateProfileMaster(ctx context.Context, req *pb.ProfileMasterProto) (*pb.ProfileMasterProto, error) {
+	userId, tenant := auth.GetUserIdAndTenant(ctx)
+
+	loginModelChan, errChan := s.db.Login(tenant).FindOneById(userId)
+	select {
+	case loginModel := <-loginModelChan:
+		if loginModel.UserType != "admin" {
+			return nil, status.Error(codes.PermissionDenied, "User with id"+userId+" don't have permission")
+		}
+	case err := <-errChan:
+		logger.Error("Failed getting login info using id: "+userId, zap.Error(err))
+		return nil, status.Error(codes.Internal, "Failed getting login info using id: "+userId)
+	}
+
+	profileMasterChan, errChain := s.db.ProfileMaster(tenant).FindOneById(req.Id)
+
+	select {
+	case profileMaster := <-profileMasterChan:
+		copier.CopyWithOption(profileMaster, req, copier.Option{IgnoreEmpty: true, DeepCopy: true})
+		profileMaster.Options = req.Options
+		err := <-s.db.ProfileMaster(tenant).Save(profileMaster)
+		if err != nil {
+			logger.Error("Internal error when saving Profile Master with id: "+profileMaster.Id(), zap.Error(err))
+			return nil, status.Error(codes.Internal, err.Error())
+		} else {
+			profileMasterProto := &pb.ProfileMasterProto{}
+			copier.Copy(profileMasterProto, profileMaster)
+			return profileMasterProto, nil
+		}
+	case err := <-errChain:
+		logger.Error("Can't update Profile Master not found with id: "+req.Id, zap.Error(err))
+		return nil, status.Error(codes.NotFound, "Can't update Profile Master not found with id: "+req.Id)
 	}
 }
